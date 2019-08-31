dddc8bf13465518d3d6b261070bd876bd59227c2
pstetz <pstetz@berkeley.edu>
2019-08-31 08:16:04 -0700

    refactor check_today to be more flexible

--- check_today.py
+++ check_today.py
@@ -1,52 +1,51 @@
+import os
+import time
 import numpy as np
 import pandas as pd
-import time
+from os.path import join
 from datetime import datetime, timedelta
 
-import warnings
-warnings.filterwarnings('ignore')
-
-PATH = "/Users/pbezuhov/git/Birthdays/"
-df = pd.read_csv(PATH + "birthdays.csv")
 
 def get_birthdays(df, day, year):
-    birthdays = df[df.birthday == day]
-    birthdays["ages"] = year - birthdays.birthyear
+    df_copy = df.copy()
+    df_copy["ages"] = year - df_copy["birthyear"]
+    birthdays = df_copy.loc[df_copy.birthday == day]
     return birthdays
 
 def format_notification(birthdays, message):
-    if len(birthdays) == 0:
-        message.append("None")
-        return message
     for person in birthdays.values:
         name = person[0]
         age  = person[3]
-        
+
         message.append(name)
         if not np.isnan(age):
             message.append(f" - {int(age)} yrs old")
         message.append("\n")
     return message
 
-def get_notification(today, tomorrow, year):
-    ### Today
-    message = list("Today's Birthdays\n")
-    birthdays_today = get_birthdays(df, today, year)
-    message = format_notification(birthdays_today, message)
-    
-    ### Tomorrow
-    message.append("\n")
-    message.append("Tomorrow's birthdays\n")
-    birthdays_tomorrow = get_birthdays(df, tomorrow, year)
-    message = format_notification(birthdays_tomorrow, message)
+def get_notification(dates, year):
+    message = list()
 
-    if len(birthdays_today) + len(birthdays_tomorrow) != 0:
-        return "".join(message)
-    return "None"
+    for name, date in dates:
+        birthdays = get_birthdays(df, date, year)
+        if len(birthdays) != 0:
+            message.append("%s's Birthdays\n" % name)
+            message = format_notification(birthdays, message)
+            message.append("\n")
+
+    return "".join(message)
 
 if __name__ == "__main__":
+    root = "/Users/pbezuhov/git/Birthdays"
+    csv  = join(root, "birthdays.csv")
+    df   = pd.read_csv(csv)
+
     today, year = time.strftime("%-d %B"), int(time.strftime("%Y"))
-    tomorrow = (datetime.strptime(today, "%d %B") + timedelta(days=1)).strftime("%-d %B")
-    message  = get_notification(today, tomorrow, year)
+    dates = [ ("Today", today) ]
+    for day_offset in range(1, 10):
+        day = (datetime.strptime(today, "%d %B") + timedelta(days=day_offset)).strftime("%-d %B")
+        dates.append( ("%d day from now" % day_offset, day) )
+
+    message = get_notification(dates, year)
     print(message)
 
